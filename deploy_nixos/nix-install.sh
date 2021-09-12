8f4e49440c4ac16b99adea82a0cf72062a2989ec
Matthew Nicholson <matt@matt-land.com>
2021-09-12 00:22:31 -0400

    try nix-portable

--- deploy_nixos/nix-install.sh
+++ deploy_nixos/nix-install.sh
@@ -6,6 +6,9 @@ nix_user_chroot=$1
 chroot_path=$2
 shift 2
 
+
+echo "downloading nix-portable" >&2
 curl -o "$nix_user_chroot" -sL https://github.com/DavHau/nix-portable/releases/download/v008/nix-portable > /dev/null
+chmod +x "$nix_user_chroot"
 
 echo '{"nix-user-chroot":"'"$nix_user_chroot"'","chroot-path":"'"$chroot_path"'"}'
